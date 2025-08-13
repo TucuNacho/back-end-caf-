792ab135d505125e759b9f00490e77aa50262f47
Nacho <barrojojoaquin2@gmail.com>
2025-08-13 20:19:15 -0300

    podemos ver los datos que llegan al console log

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -7,7 +7,18 @@ export const leerProducto=(req, res)=>{
 
 }
 //agregar funcion para crear productos
-export const crearProducto=(req, res)=>{
-    
+export const crearProducto= async (req, res)=>{
+  try {
+     //1- recibir el objeto que tengo que agregar a la BD
+     console.log(req.body);
+
+    //2- validar los datos del objeto
+    //-3 guardar el objeto en la base de datos
+    //-4 enviar respuestas
+  } catch (error) {
+    console.error(error);
+
+  }
+
 }
 //agregar funcion para editar productos
